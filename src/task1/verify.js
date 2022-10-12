f224bb0246e7df9abe227359d6ac0f631f77604b
Gizmo <gizmo.dev.dev@gmail.com>
2022-10-12 13:00:48 +0530

    solve task 1

--- src/task1/verify.js
+++ src/task1/verify.js
@@ -1,4 +1,27 @@
-const verify = () => {
+const openedBrackets = ['[', '(', '<'];
+const closedBrackets = [']', ')', '>'];
+const bracketsMap = {
+  ']': '[',
+  ')': '(',
+  '>': '<'
+}
+
+const verify = (str) => {
+  const bracketStack = [];
+  for (let i = 0; i < str.length; i++) {
+    const symbol = str[i];
+    if (openedBrackets.some((bracket) => bracket === symbol)) {
+      bracketStack.push(symbol);
+      continue;
+    }
+    if (closedBrackets.some((bracket) => bracket === symbol)) {
+      if (bracketStack.length === 0) return 0;
+      const correctBracket = bracketStack.pop();
+      if (correctBracket !== bracketsMap[symbol]) return 0;
+    }
+  }
+
+  return bracketStack.length === 0 ? 1 : 0;
 }
 
 module.exports = verify;
