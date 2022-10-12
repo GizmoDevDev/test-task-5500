7448e39098f52b35b9655e635247c2b263d33714
Gizmo <gizmo.dev.dev@gmail.com>
2022-10-12 13:23:03 +0530

    task 2 refactor

--- src/task2/func.js
+++ src/task2/func.js
@@ -1,37 +1,11 @@
 const func = (s, a, b) => {
-  var match_empty=/^$/ ;
-  if (s.match(match_empty)) {
-    return -1;
-  } else {
-    var i=s.length-1;
-    var aIndex=-1;
-    var bIndex=-1;
+  let i = s.length-1;
 
-    while ((aIndex==-1) && (bIndex==-1) && (i>=0))
-    {
-      if (s.substring(i, i+1) == a)
-        aIndex=i;
-      if (s.substring(i, i+1) == b)
-        bIndex=i;
-
-      i--;
-    }
-
-    if (aIndex != -1)
-    {
-      if (bIndex == -1)
-        return aIndex;
-      else
-        return Math.max(aIndex, bIndex);
-    }
-    else
-    {
-      if (bIndex != -1)
-        return bIndex;
-      else
-        return -1;
-    }
+  while (i >= 0) {
+    if (s[i] === a || s[i] === b) break;
+    i--;
   }
+  return i;
 }
 
 module.exports = func;
